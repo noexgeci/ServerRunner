cda45a28a0ecd3c8c16475672aeda67bcc0dffc2
noexskid <86523746+noexskid@users.noreply.github.com>
2022-06-14 22:53:57 +0200

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,10 @@
+@echo off
+pip install pick
+pip install colorama
+pip install os
+pip install psutil
+pip install time
+pip install subprocess 
+pip install shutil
+echo Done! Run the ServerRunner.exe file
+pause
